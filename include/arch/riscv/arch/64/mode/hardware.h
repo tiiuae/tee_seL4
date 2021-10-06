450feb34365dfc89d39cc3c6320a582668f14f54
Tero Salminen <tero.salminen@unikie.com>
2021-10-06 11:39:27 +0300

    Configure kernel ELF start address
    Use KERNEL_ELF_PADDR_BASE_START for configuring kernel ELF physical
    start address. If it is not defined use default address 0x4000000.
    
    Signed-off-by: Tero Salminen <tero.salminen@unikie.com>
    Change-Id: I747e7b5892f6fd1cf469a83abace057da431973b

--- include/arch/riscv/arch/64/mode/hardware.h
+++ include/arch/riscv/arch/64/mode/hardware.h
@@ -92,7 +92,10 @@
 /* This represents the physical address that the kernel image will be linked to. This needs to
  * be on a 1gb boundary as we currently require being able to creating a mapping to this address
  * as the largest frame size */
-#define KERNEL_ELF_PADDR_BASE (physBase + UL_CONST(0x4000000))
+#ifndef KERNEL_ELF_PADDR_BASE_START
+#define KERNEL_ELF_PADDR_BASE_START 0x4000000
+#endif /* KERNEL_ELF_PADDR_BASE_START */
+#define KERNEL_ELF_PADDR_BASE (physBase + UL_CONST(KERNEL_ELF_PADDR_BASE_START))
 
 /* The base address in virtual memory to use for the kernel ELF mapping */
 #define KERNEL_ELF_BASE (PPTR_TOP + (KERNEL_ELF_PADDR_BASE & MASK(30)))
